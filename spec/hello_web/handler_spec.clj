b18f1e8e806d6e59aad43531880b1bcd664b9e2a
Jeff Ramnani <jramnani@8thlight.com>
2016-02-03 13:52:36 -0600

    Provide some sample speclj tests.

--- spec/hello_web/handler_spec.clj
+++ spec/hello_web/handler_spec.clj
@@ -1,14 +1,17 @@
 (ns hello-web.handler-spec
   (:require [speclj.core :refer :all]
+            [ring.mock.request :refer :all]
             [hello-web.handler :refer :all]))
 
-(describe "Truth"
+(describe "Testing the web app"
 
-  (it "is true"
-    (should true))
+  (it "GET '/' should say Hello World."
+      (let [response (app (request :get "/"))]
+        (should (.contains (:body response) "Hello World"))
+        (should= 200 (:status response))))
 
-  (it "is not false"
-    (should-not false)))
-
+  (it "GET '/invalid' should say Not Found"
+      (let [response (app (request :get "/invalid"))]
+        (should= 404 (:status response)))))
 
 (run-specs)
