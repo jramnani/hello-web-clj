cc88f26f884723af4b2b5a515a13fd9c7bbbd7c1
Jeff Ramnani <jramnani@8thlight.com>
2016-02-03 11:00:37 -0600

    Reformat dev dependencies.

--- project.clj
+++ project.clj
@@ -15,4 +15,6 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.4.0"] [speclj "3.3.1"]]}})
+   {:dependencies [[ring-mock "0.1.5"]
+                   [ring/ring-devel "1.4.0"]
+                   [speclj "3.3.1"]]}})
