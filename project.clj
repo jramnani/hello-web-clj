dffb6f574948205672a84ded8da664df6c3d952d
Jeff Ramnani <jramnani@8thlight.com>
2016-02-03 10:58:10 -0600

    Add dependency on speclj.
    Now people can test using either core.test or speclj.

--- project.clj
+++ project.clj
@@ -15,4 +15,4 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.4.0"]]}})
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.4.0"] [speclj "3.3.1"]]}})
